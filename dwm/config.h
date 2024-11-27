03d2713f30d5755e756ecda4bc6716f418725a1e
will <wjopling@gmail.com>
2024-11-27 17:37:19 +1100

    Update keybinds for terminal and program exit

--- dwm/config.h
+++ dwm/config.h
@@ -73,15 +73,16 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
+
+	/* modifier                     key        function        argument */
 	{ 0,	     XF86XK_AudioLowerVolume,	   volset,	   {.v = "-5%"      } },
 	{ 0,	     XF86XK_AudioRaiseVolume,	   volset,	   {.v = "+5%"      } },
 	{ 0,	     XF86XK_AudioRaiseVolume,	   volset,	   {.v = "toggle"   } },
 	{ 0,	     XF86XK_MonBrightnessUp,	   spawn,	   {.v = light_up   } },
 	{ 0,	     XF86XK_MonBrightnessDown,	   spawn,	   {.v = light_down } },
 
-	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -89,9 +90,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
